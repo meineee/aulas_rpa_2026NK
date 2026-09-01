4259100f4d8ba58cab489c46df129a997482faf0
meineee <kawasakinicolas06@gmail.com>
2026-09-01 20:53:51 -0300

    lab02: entrega RA 6326175

---
+++ AULA02/validador_transacoes.py
@@ -0,0 +1,11 @@
+transacoes = [150.0, 3200.5, 12500.0, 450.0, -50.0, 800.0, 0]
+
+for valor in transacoes:
+    if valor > 10000.0:
+        print(f"[ALERTA] Transação suspeita de R$ <VALOR>: Encaminhada para auditoria.")
+        continue
+    elif valor <= 0:
+        print(f"[ERRO CRÍTICO] Transação inválida encontrada (R$ <VALOR>). Interrompendo bot...")
+        break
+    else:
+        print(f"[SUCESSO] Transação de R$ <VALOR> processada.")
