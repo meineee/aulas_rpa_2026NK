8f7f3c12c992523ca61c4ac391af8eb27dc61487
meineee <kawasakinicolas06@gmail.com>
2026-09-01 23:19:00 -0300

    lab03: entrega RA 6326175

---
+++ AULA03/main.py
@@ -0,0 +1,32 @@
+from mod_rh import cadastrar_colaborador, exibir_colaboradores
+
+colaboradores = []
+
+while True:
+    print("\nSistema de RH")
+    print("1 - Cadastrar colaborador")
+    print("2 - Listar colaboradores")
+    print("0 - Sair")
+
+    opcao = input("Escolha uma opcao: ")
+
+    if opcao == "1":
+        nome = input("Nome: ")
+        cargo = input("Cargo: ")
+        while True:
+            try:
+                salario = float(input("Salario: ").replace(",", "."))
+                break
+            except ValueError:
+                print("Valor inválido. Digite um número (ex: 3500.00).")
+        colaborador = cadastrar_colaborador(nome, cargo, salario)
+        colaboradores.append(colaborador)
+        print(f"Colaborador '{nome}' cadastrado com sucesso!")
+
+    elif opcao == "2":
+        exibir_colaboradores(colaboradores)
+    elif opcao == "0":
+        print("Sistema encerrado.")
+        break
+    else:
+        print("Opcao invalida.")
---
+++ AULA03/mod_rh.py
@@ -0,0 +1,18 @@
+def cadastrar_colaborador(nome: str, cargo: str, salario: float) -> dict:
+    return {
+        "nome": nome,
+        "cargo": cargo,
+        "salario": salario,
+    }
+
+def exibir_colaboradores(lista_colaboradores: list) -> None:
+    if not lista_colaboradores:
+        print("Nenhum colaborador cadastrado.")
+        return
+
+    print("\nLista de Colaboradores")
+    for i, colaborador in enumerate(lista_colaboradores, start=1):
+        print(f"{i}. Nome: {colaborador['nome']}")
+        print(f"Cargo: {colaborador['cargo']}")
+        print(f"Salario: R$ {colaborador['salario']:.2f}")
+    print("\n")
