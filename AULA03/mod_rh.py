2a7d9c0ce45e68ca50726cd71c70dca76ae7ed6f
meineee <kawasakinicolas06@gmail.com>
2026-09-08 20:05:19 -0300

    Update mod_rh.py

--- AULA03/mod_rh.py
+++ AULA03/mod_rh.py
@@ -4,12 +4,10 @@ def cadastrar_colaborador(nome: str, cargo: str, salario: float) -> dict:
         "cargo": cargo,
         "salario": salario,
     }
-
 def exibir_colaboradores(lista_colaboradores: list) -> None:
     if not lista_colaboradores:
         print("Nenhum colaborador cadastrado.")
         return
-
     print("\nLista de Colaboradores")
     for i, colaborador in enumerate(lista_colaboradores, start=1):
         print(f"{i}. Nome: {colaborador['nome']}")
